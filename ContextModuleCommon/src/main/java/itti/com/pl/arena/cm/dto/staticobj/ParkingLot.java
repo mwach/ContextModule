531228ee2b2b6cbb7cce47df79c62117e4300cf4
mwach <mwach@wp.pl>
2014-01-18 10:03:21 +0100

    common refactoring

---
+++ ContextModuleCommon/src/main/java/itti/com/pl/arena/cm/dto/staticobj/ParkingLot.java
@@ -0,0 +1,94 @@
+package itti.com.pl.arena.cm.dto.staticobj;
+
+import itti.com.pl.arena.cm.dto.GeoObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Implementation of the {@link GeoObject} representing parking lot
+ * 
+ * @author cm-admin
+ * 
+ */
+public class ParkingLot extends GeoObject {
+
+    /**
+	 * 
+	 */
+    private static final long serialVersionUID = 1L;
+
+    /*
+     * list if infrastructure objects located on given parking
+     */
+    private Map<String, Infrastructure> infrastructures = new HashMap<String, Infrastructure>();
+    /*
+     * list if buildings located on given parking
+     */
+    private Map<String, Building> buildings = new HashMap<String, Building>();
+
+    /**
+     * Constructor of that class
+     * 
+     * @param id
+     *            ID of the object
+     */
+    public ParkingLot(String id) {
+        super(id);
+    }
+
+    /**
+     * Returns list of infrastructure objects located on given parking
+     * 
+     * @return
+     */
+    public Map<String, Infrastructure> getInfrastructure() {
+        return new HashMap<>(infrastructures);
+    }
+
+    /**
+     * Adds a new infrastructure object located on given parking
+     * 
+     * @param infrastructure
+     */
+    public void addIntrastructure(Infrastructure infrastructure) {
+        this.infrastructures.put(infrastructure.getId(), infrastructure);
+    }
+
+    /**
+     * Removes existing infrastructure object from given parking
+     * 
+     * @param infrastructureId
+     */
+    public void removeIntrastructure(String infrastructureId) {
+        this.infrastructures.remove(infrastructureId);
+    }
+
+    /**
+     * Returns list of buildings located on given parking
+     * 
+     * @return
+     */
+    public Map<String, Building> getBuildings() {
+        return new HashMap<>(buildings);
+    }
+
+    /**
+     * Adds a new building object located on given parking
+     * 
+     * @param building
+     */
+    public void addBuilding(Building building) {
+        this.buildings.put(building.getId(), building);
+    }
+
+    /**
+     * Removes existing building object from given parking
+     * 
+     * @param buildingId
+     */
+    public void removeBuilding(String buildingId) {
+        this.buildings.remove(buildingId);
+    }
+
+}
