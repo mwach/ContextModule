57b43ab366392e52cfb12e6cf7e474021ef027b3
mwach <mwach@wp.pl>
2014-02-24 20:57:31 +0100

    new interface for platform changes

---
+++ ContextModuleServer/src/main/java/itti/com/pl/arena/cm/service/PlatformListener.java
@@ -0,0 +1,18 @@
+package itti.com.pl.arena.cm.service;
+
+import itti.com.pl.arena.cm.dto.Location;
+import itti.com.pl.arena.cm.location.LocationListener;
+
+/**
+ * Listener used to inform all the components, that platform had reached the destination
+ * @author cm-admin
+ *
+ */
+public interface PlatformListener {
+
+    /**
+     * method called by the {@link LocationListener} once the destination will be reached
+     * @param location final position of the platform
+     */
+    public void destinationReached(Location location);
+}
--- ContextModuleServer/src/main/java/itti/com/pl/arena/cm/service/PlatformTracker.java
+++ ContextModuleServer/src/main/java/itti/com/pl/arena/cm/service/PlatformTracker.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Required;
 import itti.com.pl.arena.cm.Service;
 import itti.com.pl.arena.cm.dto.GeoObject;
 import itti.com.pl.arena.cm.dto.Location;
-import itti.com.pl.arena.cm.geoportal.GeoportalException;
-import itti.com.pl.arena.cm.geoportal.gov.pl.GeoportalService;
 import itti.com.pl.arena.cm.location.LocationListener;
 import itti.com.pl.arena.cm.location.Range;
 import itti.com.pl.arena.cm.ontology.Ontology;
@@ -33,12 +31,10 @@ public class PlatformTracker implements Service, LocationListener {
     private Persistence persistence = null;
     // ontology module used to store/retrieve platform information
     private Ontology ontology = null;
-    // geoportal module used to collect geoportal information
-    private GeoportalService geoportal = null;
-    // radius used for geoportal search purposes
-    private double radius = 0.0;
-    // maximum break/parking time. After that time, CM will assume, that platform is on the parking
-    private long maxBreakTime = 30;
+    // platform listener to be notified once platform will reach its destination
+    private PlatformListener platformListener = null;
+    // max idle time: after that time passes, destinationReached of the platformListener will be called
+    private int maxIdleTime = 0;
 
     /*
      * ID of the listener (if not provided, random UUID generated during initialization)
@@ -62,40 +58,22 @@ public class PlatformTracker implements Service, LocationListener {
         this.persistence = persistence;
     }
 
-    private Ontology getOntology() {
-        return ontology;
+    private PlatformListener getPlatformListener() {
+        return platformListener;
     }
 
     @Required
-    public void setOntology(Ontology ontology) {
-        this.ontology = ontology;
+    public void setPlatformListener(PlatformListener platformListener) {
+        this.platformListener = platformListener;
     }
 
-    private GeoportalService getGeoportal() {
-        return geoportal;
-    }
-
-    @Required
-    public void setGeoportal(GeoportalService geoportal) {
-        this.geoportal = geoportal;
-    }
-
-    private double getRadius() {
-        return radius;
+    private int getMaxIdleTime() {
+        return maxIdleTime;
     }
 
     @Required
-    public void setRadius(double radius) {
-        this.radius = radius;
-    }
-
-    private long getMaxBreakTime() {
-        return maxBreakTime;
-    }
-
-    @Required
-    public void setMaxBreakTime(long maxBreakTime) {
-        this.maxBreakTime = maxBreakTime;
+    public void setMaxIdleTime(int maxIdleTime) {
+        this.maxIdleTime = maxIdleTime;
     }
 
     /*
@@ -136,69 +114,43 @@ public class PlatformTracker implements Service, LocationListener {
             LogHelper.warning(PlatformTracker.class, "onLocationChange",
                     "Could not persist location data: for platform %s. Details: %s", getPlatformId(), e.getLocalizedMessage());
         }
-        try {
-            //check all the ranges, from the widest one to the closest
-            for (Range range : Range.values()) {
-
-                //get parking lots for given range
-                Set<GeoObject> parkingLots = ontology.getGISObjects(location.getLongitude(), location.getLatitude(), range.getRangeInKms(),
-                        OntologyConstants.Parking.name());
-
-                if (!parkingLots.isEmpty()) {
-                    LogHelper.debug(PlatformTracker.class, "onLocationChange",
-                            "Found parking lot in the close range: %f. Parking details: %s", range.getRangeInKms(), parkingLots);
-
-                    //we are on the parking
-                    if(range == Range.getClosestRange()){
-                        //TODO: do we want to do something with that?
-                    }
-                }else{
-                    //no parking lots in the given area, break the loop
-                    break;
-                }
-            }
-
-        } catch (OntologyException exc) {
-            LogHelper.warning(PlatformTracker.class, "onLocationChange",
-                    "Could not find parking lots in the given area. Details: %s", exc.getLocalizedMessage());
-        }
     }
 
-    public void checkIfMoving() {
+    public void checkPlatformStopped() {
 
-        LogHelper.debug(PlatformTracker.class, "checkIfMoving", "check, if platform %s is moving", getPlatformId());
         try {
-            // get the last location of the object
+
             Location lastLocation = getPersistence().getLastPosition(getPlatformId());
-            // check if object is moving
-            if (lastLocation != null && lastLocation.getSpeed() < 0.01) {
-                // if not - check, when it was moving for the last time
-                LogHelper.debug(PlatformTracker.class, "checkIfMoving", "platform %s is not moving for last timestamp: %d",
-                        getPlatformId(), lastLocation.getTime());
+            if (DateTimeHelper.delta(lastLocation.getTime(), System.currentTimeMillis() / 1000, DateTimeHelper.SECOND) > getMaxIdleTime()) {
 
-                // check, for how long platform is not moving
-                long pauseTime = DateTimeHelper.delta(System.currentTimeMillis(), lastLocation.getTime(), DateTimeHelper.MINUTE);
+                // check all the ranges, from the widest one to the closest
+                for (Range range : Range.values()) {
 
-                // if MaxBreakTime will be reached, assume that parking was reached
-                // note double condition here - it's here to avoid calling geoportal service every function call
-                if (pauseTime > getMaxBreakTime() && pauseTime < (getMaxBreakTime() + 1)) {
+                    // get parking lots for given range
+                    Set<GeoObject> parkingLots = ontology.getGISObjects(lastLocation.getLongitude(), lastLocation.getLatitude(),
+                            range.getRangeInKms(), OntologyConstants.Parking.name());
 
-                    LogHelper.debug(PlatformTracker.class, "checkIfMoving", "platform %s is not moving for last %d minutes",
-                            getPlatformId(), getMaxBreakTime());
+                    if (!parkingLots.isEmpty()) {
+                        LogHelper.debug(PlatformTracker.class, "checkPlatformStopped",
+                                "Found parking lot in the close range: %f. Parking details: %s", range.getRangeInKms(),
+                                parkingLots);
 
-                    // check, if there is data in ontology collected for given location
-                    Set<GeoObject> ontoData = getOntology().getGISObjects(lastLocation.getLongitude(), lastLocation.getLatitude(), getRadius());
-
-                    // if there is no data, try to download it from geoportal
-                    if (ontoData.isEmpty()) {
-                        Set<GeoObject> geoportalData = getGeoportal().getGeoportalData(lastLocation, getRadius());
-                        // store downloaded data into ontology
-                        getOntology().updateGeoportalData(lastLocation.getLongitude(), lastLocation.getLatitude(), geoportalData);
+                        // we are on the parking
+                        if (range == Range.getClosestRange()) {
+                            getPlatformListener().destinationReached(lastLocation);
+                        }
+                    } else {
+                        // no parking lots in the given area, break the loop
+                        break;
                     }
                 }
             }
-        } catch (OntologyException | GeoportalException | PersistenceException exc) {
-            LogHelper.exception(PlatformTracker.class, "checkIfMoving", "Could not calculate if object is still moving.", exc);
+        } catch (OntologyException exc) {
+            LogHelper.warning(PlatformTracker.class, "checkPlatformStopped",
+                    "Could not find parking lots in the given area. Details: %s", exc.getLocalizedMessage());
+        } catch (PersistenceException exc) {
+            LogHelper.warning(PlatformTracker.class, "checkPlatformStopped",
+                    "Could not retrieve parking data from the persistence storage. Details: %s", exc.getLocalizedMessage());
         }
     }
 
