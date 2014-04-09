dc8cb674b36a77ea8b96524fe15a52721a33331e
mwach <mwach@wp.pl>
2014-04-09 20:26:19 +0200

    ui stubs

---
+++ ContextModuleClient/src/main/java/itti/com/pl/arena/cm/client/ui/MainUI.java
@@ -0,0 +1,13 @@
+package itti.com.pl.arena.cm.client.ui;
+
+import javax.swing.JFrame;
+
+public class MainUI extends JFrame {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    
+}
