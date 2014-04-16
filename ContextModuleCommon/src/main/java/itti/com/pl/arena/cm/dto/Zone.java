d77be73392d0471166c500bb6c7496b967fc0476
mwach <mwach@wp.pl>
2014-04-16 13:41:31 +0100

    support for zones in cm ui

---
+++ ContextModuleCommon/src/main/java/itti/com/pl/arena/cm/dto/Zone.java
@@ -0,0 +1,117 @@
+package itti.com.pl.arena.cm.dto;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Class representing ARENA Zone object
+ * @author cs-admin
+ *
+ */
+public class Zone extends OntologyObject implements Iterable<Location>{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	//<Zone ident="54" name="Zn54_gr4" plane_name="ground">
+	private long ident;
+	private String planeName;
+
+	/*
+	 * list of zone coordinates
+	 */
+	private List<Location> coordinates = new ArrayList<>();
+
+	/**
+	 * Creates a new zone
+	 * @param name ID/name of the zone
+	 */
+	public Zone(String name) {
+		super(name);
+	}
+
+	/**
+	 * Adds a new coordinate to the zone
+	 * @param coordinate new coordinate
+	 */
+	public void addCoordinate(Location coordinate){
+		if(coordinate != null){
+			coordinates.add(coordinate);
+		}
+	}
+
+	/**
+	 * Adds a list of new coordinates to the zone
+	 * @param coordinates list of coordinates
+	 */
+	public void addCoordinates(Collection<Location> coordinates){
+		if(coordinates != null){
+			coordinates.addAll(coordinates);
+		}
+	}
+
+	/**
+	 * Removes coordinate from the zone
+	 * @param coordinate coordinate to remove
+	 * @return true, if coordinate was removed, false otherwise
+	 */
+	public boolean removeCoordinate(Location coordinate){
+		if(coordinate != null){
+			return coordinates.remove(coordinate);
+		}
+		return false;
+	}
+
+	@Override
+	public Iterator<Location> iterator() {
+		return coordinates.iterator();
+	}
+
+	/**
+	 * Returns unique identifier of the zone
+	 * @return zone Id
+	 */
+	public long getIdent() {
+		return ident;
+	}
+
+	/**
+	 * Sets unique identifier of the zone
+	 * @param ident zone ID
+	 */
+	public void setIdent(long ident) {
+		this.ident = ident;
+	}
+
+	/**
+	 * Returns name of the zone
+	 * @return zone name
+	 */
+	public String getName() {
+		return getId();
+	}
+
+	/**
+	 * Returns name of the plane
+	 * @return plane name
+	 */
+	public String getPlaneName() {
+		return planeName;
+	}
+
+	/**
+	 * Sets name of the plane
+	 * @param planeName plane name
+	 */
+	public void setPlaneName(String planeName) {
+		this.planeName = planeName;
+	}
+
+	public Location[] getLocations() {
+		return coordinates.toArray(new Location[coordinates.size()]);
+	}
+}
---
+++ ContextModuleCommon/src/main/java/itti/com/pl/arena/cm/utils/helper/ArenaObjectsMapper.java
@@ -0,0 +1,80 @@
+package itti.com.pl.arena.cm.utils.helper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import eu.arena_fp7._1.AbstractNamedValue;
+import eu.arena_fp7._1.Object;
+import eu.arena_fp7._1.ObjectFactory;
+import eu.arena_fp7._1.RealWorldCoordinate;
+import eu.arena_fp7._1.SimpleNamedValue;
+import itti.com.pl.arena.cm.dto.Location;
+import itti.com.pl.arena.cm.dto.Zone;
+import itti.com.pl.arena.cm.service.MessageConstants.ContextModuleRequestProperties;
+
+/**
+ * Translates ARENA DTO objects into CM internal objects
+ * @author cs-admin
+ *
+ */
+public final class ArenaObjectsMapper {
+
+	private ArenaObjectsMapper(){}
+	private static ObjectFactory objectFactory = new ObjectFactory();
+
+	public static Location fromLocation(eu.arena_fp7._1.Location arenaLocation){
+		return new Location(arenaLocation.getX(), arenaLocation.getY());
+	}
+
+	public static Location fromLocation(eu.arena_fp7._1.RealWorldCoordinate arenaLocation){
+		return new Location(arenaLocation.getX(), arenaLocation.getY(), 0, arenaLocation.getZ());
+	}
+
+	public static eu.arena_fp7._1.Location toLocation(Location location){
+		eu.arena_fp7._1.Location arenaLocation = objectFactory.createLocation();
+		arenaLocation.setX(location.getLongitude());
+		arenaLocation.setY(location.getLatitude());
+		return arenaLocation;
+	}
+
+	public static eu.arena_fp7._1.RealWorldCoordinate toRealWorldCoordinate(Location location){
+		eu.arena_fp7._1.RealWorldCoordinate arenaLocation = objectFactory.createRealWorldCoordinate();
+		arenaLocation.setX(location.getLongitude());
+		arenaLocation.setY(location.getLatitude());
+		arenaLocation.setZ(location.getAltitude());
+		return arenaLocation;
+	}
+
+	public static Zone fromZoneObject(Object response) {
+
+		String zoneName = null;
+		String planeName = null;
+		long ident = 0;
+		List<Location> coordinates = new ArrayList<>();
+
+		for (AbstractNamedValue feature : response.getFeatureVector().getFeature()) {
+
+			if(feature instanceof SimpleNamedValue){
+				String name = ((SimpleNamedValue)feature).getFeatureName();
+				String value = ((SimpleNamedValue)feature).getValue();
+				if(StringHelper.equalsIgnoreCase(ContextModuleRequestProperties.name.name(), name)){
+					zoneName = value;
+				}else if(StringHelper.equalsIgnoreCase(ContextModuleRequestProperties.plane_name.name(), name)){
+					planeName = value;
+				} else if(StringHelper.equalsIgnoreCase(ContextModuleRequestProperties.ident.name(), name)){
+					ident = NumbersHelper.getIntegerFromString(value);
+				}
+
+			}else if(feature instanceof RealWorldCoordinate){
+				coordinates.add(ArenaObjectsMapper.fromLocation((RealWorldCoordinate)feature));
+			}
+		}
+
+		Zone zone = new Zone(zoneName);
+		zone.setPlaneName(planeName);
+		zone.setIdent(ident);
+		zone.addCoordinates(coordinates);
+		return zone;
+	}
+
+}
