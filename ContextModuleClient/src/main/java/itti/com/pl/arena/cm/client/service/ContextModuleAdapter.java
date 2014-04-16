a8fc4a9193503f249cd8ce771f4034614681a890
mwach <mwach@wp.pl>
2014-04-16 07:57:21 +0100

    support for cm adapter for ui

--- ContextModuleClient/src/main/java/itti/com/pl/arena/cm/client/service/ContextModuleAdapter.java
+++ ContextModuleClient/src/main/java/itti/com/pl/arena/cm/client/service/ContextModuleAdapter.java
@@ -5,12 +5,14 @@ import java.util.List;
 
 import itti.com.pl.arena.cm.service.LocalContextModule;
 import itti.com.pl.arena.cm.service.MessageConstants.ContextModuleRequests;
+import itti.com.pl.arena.cm.utils.helper.LocationHelper;
 
 import com.safran.arena.impl.ModuleImpl;
 
 import eu.arena_fp7._1.AbstractDataFusionType;
 import eu.arena_fp7._1.FeatureVector;
 import eu.arena_fp7._1.Location;
+import eu.arena_fp7._1.RealWorldCoordinate;
 import eu.arena_fp7._1.SimpleNamedValue;
 
 /**
@@ -21,64 +23,82 @@ import eu.arena_fp7._1.SimpleNamedValue;
  */
 public class ContextModuleAdapter {
 
-    private String moduleName;
-    private LocalContextModule contextModule;
-    private boolean connected = false;
+	private String moduleName;
+	private LocalContextModule contextModule;
+	private boolean connected = false;
 
-    public ContextModuleAdapter(String moduleName){
-        this.moduleName = moduleName;
-    }
+	public ContextModuleAdapter(String moduleName) {
+		this.moduleName = moduleName;
+	}
 
-    public void connect(String brokerUrl){
-        if(! connected){
-            contextModule = new ContextModuleFacade(moduleName, brokerUrl);
-            contextModule.init();
-            connected = true;
-        }
-    }
+	public void connect(String brokerUrl) {
+		if (!connected) {
+			contextModule = new ContextModuleFacade(moduleName, brokerUrl);
+			contextModule.init();
+			connected = true;
+		}
+	}
 
-    public void disconnect(){
-        if(connected){
-            contextModule.shutdown();
-            connected = false;
-        }
-    }
-    public boolean isConnected(){
-        return connected;
-    }
+	public void disconnect() {
+		if (connected) {
+			contextModule.shutdown();
+			connected = false;
+		}
+	}
 
-    public List<String> getListOfParkingLots() {
-        SimpleNamedValue request = contextModule.createSimpleNamedValue(moduleName, null);
-        request.setHref(ContextModuleRequests.getListOfParkingLots.name());
-        eu.arena_fp7._1.Object response = contextModule.getListOfParkingLots(request);
-        return getStringsFromFeatureVector(response == null ? null : response.getFeatureVector());
-    }
+	public boolean isConnected() {
+		return connected;
+	}
 
-    public List<String> getListOfZones(String parkingLot) {
-        SimpleNamedValue request = contextModule.createSimpleNamedValue(moduleName, parkingLot);
-        request.setHref(ContextModuleRequests.getListOfZones.name());
-        eu.arena_fp7._1.Object response = contextModule.getListOfZones(request);
-        return getStringsFromFeatureVector(response == null ? null : response.getFeatureVector());
-    }
+	public List<String> getListOfParkingLots() {
+		SimpleNamedValue request = contextModule.createSimpleNamedValue(
+				moduleName, null);
+		request.setHref(ContextModuleRequests.getListOfParkingLots.name());
+		eu.arena_fp7._1.Object response = contextModule
+				.getListOfParkingLots(request);
+		return getStringsFromFeatureVector(response == null ? null : response
+				.getFeatureVector());
+	}
 
-    /**
-     * Parses response stored inside {@link FeatureVector} object
-     * 
-     * @param featureVector
-     *            information stored inside {@link FeatureVector}
-     */
-    private List<String> getStringsFromFeatureVector(FeatureVector featureVector) {
- 
-        List<String> response = new ArrayList<>();
-        if (featureVector != null) {
-            for (AbstractDataFusionType feature : featureVector.getFeature()) {
-                if (feature instanceof Location) {
-                } else {
-                    response.add(((SimpleNamedValue) feature).getValue());
-                }
-            }
-        }
-        return response;
-    }
+	public List<String> getListOfZones(String parkingLot) {
+		SimpleNamedValue request = contextModule.createSimpleNamedValue(
+				moduleName, parkingLot);
+		request.setHref(ContextModuleRequests.getListOfZones.name());
+		eu.arena_fp7._1.Object response = contextModule.getListOfZones(request);
+		return getStringsFromFeatureVector(response == null ? null : response
+				.getFeatureVector());
+	}
+
+	/**
+	 * Parses response stored inside {@link FeatureVector} object
+	 * 
+	 * @param featureVector
+	 *            information stored inside {@link FeatureVector}
+	 */
+	private List<String> getStringsFromFeatureVector(FeatureVector featureVector) {
+
+		List<String> response = new ArrayList<>();
+		if (featureVector != null) {
+			for (AbstractDataFusionType feature : featureVector.getFeature()) {
+				if (feature instanceof RealWorldCoordinate) {
+					response.add(LocationHelper
+							.createStringFromLocation(new itti.com.pl.arena.cm.dto.Location(
+									((RealWorldCoordinate) feature).getX(),
+									((RealWorldCoordinate) feature).getY(), 0,
+									((RealWorldCoordinate) feature).getZ())));
+				} else {
+					if (feature instanceof Location) {
+						response.add(LocationHelper
+								.createStringFromLocation(new itti.com.pl.arena.cm.dto.Location(
+										((Location) feature).getX(),
+										((Location) feature).getY())));
+					} else {
+						response.add(((SimpleNamedValue) feature).getValue());
+					}
+				}
+			}
+		}
+		return response;
+	}
 
 }
