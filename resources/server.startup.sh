ad72c732bdb725e5c3dc61d6c145de63dca5a594
mwach <mwach@wp.pl>
2014-04-27 19:42:00 +0200

    new arenabus init script

--- resources/server.startup.sh
+++
@@ -1,4 +0,0 @@
-LD_LIBRARY_PATH=/usr/local/lib
-export LD_LIBRARY_PATH=/usr/local/lib
-
-java -cp libs/arena.jar:libs/ServicePlatformInterface.jar:/usr/local/share/java/zmq.jar  com.safran.arena.impl.Server
